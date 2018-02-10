703d9fb6d0d3ade88020d784cf013c35d89e6049
Rashmi Singh <singhrashmi579@gmail.com>
2018-02-11 04:18:26 +0530

    send kafka msgs to client using socketio server

--- src/main/java/com/rashmi/message/Notifier.java
+++ src/main/java/com/rashmi/message/Notifier.java
@@ -1,6 +1,7 @@
 package com.rashmi.message;
 
 import com.rashmi.kafka.MyConsumer;
+import com.rashmi.socketio.SocketServer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -10,12 +11,15 @@ import java.util.Arrays;
 public class Notifier {
 
     public static void main(String[] args){
+        SocketServer server = new SocketServer();
         MyConsumer consumer  = new MyConsumer();
         KafkaConsumer<String, String> kafkaConsumer = consumer.getConsumer();
         kafkaConsumer.subscribe(Arrays.asList("test"));
         while(true){
             ConsumerRecords<String, String> records = kafkaConsumer.poll(200);
             for(ConsumerRecord<String, String> record: records){
+
+                server.getServer().getRoomOperations("clientName").sendEvent("progressData", record);
                 System.out.println(record.value());
 
             }
