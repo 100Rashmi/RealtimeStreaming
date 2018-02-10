9b4f73a05b4d54a1883b5c8e957896664ab75f22
Rashmi Singh <singhrashmi579@gmail.com>
2018-02-11 04:17:33 +0530

    add socket.io server connection

---
+++ src/main/java/com/rashmi/socketio/SocketServer.java
@@ -0,0 +1,22 @@
+package com.rashmi.socketio;
+
+
+import com.corundumstudio.socketio.Configuration;
+import com.corundumstudio.socketio.SocketIOServer;
+
+public class SocketServer {
+
+   SocketIOServer server;
+   public SocketServer(){
+       Configuration config = new Configuration();
+       config.setHostname("localhost");
+       config.setPort(9050);
+
+       server = new SocketIOServer(config);
+
+   }
+
+   public SocketIOServer getServer(){
+       return server;
+   }
+}
